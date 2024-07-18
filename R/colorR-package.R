90e9a1265c8a41d803835518fe36e3e1f6e864a8
Marleen Fiederer <fiederer.marleen@gmail.com>
2024-07-19 00:51:59 +0200

    added documentation

---
+++ R/colorR-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom graphics rect
+#' @importFrom graphics text
+#' @importFrom grDevices col2rgb
+#' @importFrom grDevices colors
+#' @importFrom methods is
+## usethis namespace: end
+NULL
